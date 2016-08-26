67a41cd94cc792a2c7007c83daac81ee49b3ac15
Maxime DAVID <contact@maximedavid.fr>
2016-08-26 21:48:29 +0200

    export colors less/sass is now working

--- js/index.js
+++ js/index.js
@@ -15,6 +15,18 @@ app.get('/load/:colors', function (request, response) {
   response.render('loadColors.ejs', { colors : request.params.colors } );
 });
 
+app.get('/export/:kind/:colors', function (request, response) {
+  if(request.params.kind === "less") {
+    response.render('exportColors.ejs', { exportArray : less(request.params.colors) } );
+  }
+  else if(request.params.kind === "sass") {
+    response.render('exportColors.ejs', { exportArray : sass(request.params.colors) } );
+  }
+  else {
+    response.status(404).send({ errorMsg: 'kind : ' + request.params.kind + ' is incorrect' });
+  }
+});
+
 app.locals.createDivColor  = function(colorName) {
   var iDiv = window.document.createElement('div');
   iDiv.className = 'block';
@@ -24,6 +36,27 @@ app.locals.createDivColor  = function(colorName) {
   iDiv.style.backgroundColor = "#" + colorName;
   iDiv.innerHTML = "#" + colorName;
   document.getElementById("result").appendChild(iDiv);
+}
+
+
+function less(colors) {
+  var res = [];
+  var colorArray = colors.split(",");
+  var length = colorArray.length;
+  for(var i=0;i<length;++i) {
+    res.push("@color" + i + ": #" + colorArray[i] + ";");
+  }
+  return res;
+}
+
+function sass(colors) {
+  var res = [];
+  var colorArray = colors.split(",");
+  var length = colorArray.length;
+  for(var i=0;i<length;++i) {
+    res.push("$color" + i + ": #" + colorArray[i] + ";");
+  }
+  return res;
 }
 
 app.listen(port);
