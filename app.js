1d908ff46ccc9983ccc9900c95edd44fdfca27e8
Aldo Ferrari <aldo@aldoferrari.com>
2016-10-01 01:37:25 +0200

    translations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express            = require("express");
-var ejs                = require('ejs');
-var path               = require('path');
+var express = require("express");
+var ejs = require('ejs');
+var path = require('path');
 
 const getColors = require("get-image-colors");
 
@@ -15,21 +15,21 @@ app.get('/', function (request, response) {
 });
 
 app.get('/fr', function (request, response) {
-  response.render('home.ejs' , getTranslateObject(request));
+  response.render('home.ejs', getTranslateObject(request));
 });
 
 app.get('/en', function (request, response) {
-  response.render('home.ejs' , getTranslateObject(request));
+  response.render('home.ejs', getTranslateObject(request));
 });
 
 app.get('/it', function (request, response) {
-  response.render('home.ejs' , getTranslateObject(request));
+  response.render('home.ejs', getTranslateObject(request));
 });
 
 
 app.get('/:lang/load/:colors', function (request, response) {
-  console.log(getTranslateObject(request, { colors : request.params.colors }));
-  response.render('loadColors.ejs', getTranslateObject(request, "colors", request.params.colors) );
+  console.log(getTranslateObject(request, { colors: request.params.colors }));
+  response.render('loadColors.ejs', getTranslateObject(request, "colors", request.params.colors));
 });
 
 app.get('/:lang/disclaimer', function (request, response) {
@@ -50,7 +50,7 @@ app.get('/screenshot', function (req, resp) {
     if (!error && response.statusCode == 200) {
       var url = "https://tenkolorsscrenshot.herokuapp.com/" + JSON.parse(body).success.replace("./", "");
       console.log("file url = " + url);
-      getColors(url, function(err, colors){
+      getColors(url, function (err, colors) {
         console.log(err);
         console.log(colors);
         var friendlyColor = transformColors(colors);
@@ -64,44 +64,44 @@ app.get('/screenshot', function (req, resp) {
 
 app.get('/screenshot/:url', function (request, response) {
   var filename = "screen_" + (new Date().getTime()) + Math.random();
-  screenshot.fromURL(request.params.url, "public/screenshots/" + filename, function(){
-    response.status(200).send({ "success" : true, "filename" : filename });
+  screenshot.fromURL(request.params.url, "public/screenshots/" + filename, function () {
+    response.status(200).send({ "success": true, "filename": filename });
   });
 })
 
 app.get('/export/:colors', function (request, response) {
 
   var colors = {
-    sass : sass(request.params.colors),
-    less : less(request.params.colors)
+    sass: sass(request.params.colors),
+    less: less(request.params.colors)
   }
 
   response.status(200).send(colors);
 });
 
 app.get('/export/:kind/:colors', function (request, response) {
-  if(request.params.kind === "less") {
+  if (request.params.kind === "less") {
     response.render('loadColors.ejs',
       getTranslateObject(
         request,
         "colors", request.params.colors,
         "exportColor", less(request.params.colors))
-      )
+    )
   }
-  else if(request.params.kind === "sass") {
+  else if (request.params.kind === "sass") {
     response.render('loadColors.ejs',
       getTranslateObject(
         request,
         "colors", request.params.colors,
         "exportColor", sass(request.params.colors))
-      )
+    )
   }
   else {
     response.status(404).send({ errorMsg: 'kind : ' + request.params.kind + ' is incorrect' });
   }
 });
 
-app.locals.createDivColor  = function(colorName) {
+app.locals.createDivColor = function (colorName) {
   var iDiv = window.document.createElement('div');
   iDiv.className = 'block';
   iDiv.style.width = "300px";
@@ -117,7 +117,7 @@ function less(colors) {
   var res = [];
   var colorArray = colors.split(",");
   var length = colorArray.length;
-  for(var i=0;i<length;++i) {
+  for (var i = 0; i < length; ++i) {
     res.push("@color" + i + ": #" + colorArray[i] + ";");
   }
   return res;
@@ -127,7 +127,7 @@ function sass(colors) {
   var res = [];
   var colorArray = colors.split(",");
   var length = colorArray.length;
-  for(var i=0;i<length;++i) {
+  for (var i = 0; i < length; ++i) {
     res.push("$color" + i + ": #" + colorArray[i] + ";");
   }
   return res;
@@ -139,22 +139,22 @@ function computeHex(col, nb) {
   var r = (parseInt(col[0], 10)).toString(16);
   var g = (parseInt(col[1], 10)).toString(16);
   var b = (parseInt(col[2], 10)).toString(16);
-  if(r.length == 1) {
-    r = "0"+r;
+  if (r.length == 1) {
+    r = "0" + r;
   }
-  if(g.length == 1) {
-    g = "0"+g;
+  if (g.length == 1) {
+    g = "0" + g;
   }
-  if(b.length == 1) {
-    b = "0"+b;
+  if (b.length == 1) {
+    b = "0" + b;
   }
-  return r+""+g+""+b;
+  return r + "" + g + "" + b;
 }
 
 function transformColors(colors) {
 
   var colorsToReturn = "";
-  for(var i=0; i<colors.length; ++i) {
+  for (var i = 0; i < colors.length; ++i) {
     colorsToReturn += computeHex(colors[i]._rgb) + ",";
   }
   colorsToReturn = colorsToReturn.substring(0, colorsToReturn.length - 1);
@@ -168,56 +168,88 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 function getTranslateObject(request, extraKey0, extraValue0, extraKey1, extraValue1) {
   var toReturn;
-  if(getLang(request) === "fr") {
+  if (getLang(request) === "fr") {
     toReturn = {
-      lang : 'fr',
-      title : '10Kolors - plus de secrets pour vos images',
-      baseline : 'Plus de Kolors secrètes dans vos images!',
-      noJs : 'JavaScript ne semble pas activé. Merci de l\'activer pour une meilleure expérience :D',
-      dropHere : 'Glissez-déposez une image ici.',
-      colorFromWebsite : 'Retrouver les couleurs d\'un site',
-      noSupportForCanvas : 'Votre navigateur ne supporte pas le tag HTML5 canvas.',
-      footerText : 'Fait avec ❤ par ',
-      forText : 'pour',
-      exportPalette : 'Exporter palette',
-      disclaimer : 'fr Disclamer',
-      about : 'fr About',
-       aboutTitle: "About 10 Kolors Project for 10KApart competition"
-    }
-  }
-  if(getLang(request) === "en") {
-    toReturn = {
-      lang : 'en',
-      title : '10Kolors - no more secrets in your images',
-      baseline : 'No more secret Kolors in your images!',
-      noJs : 'You don\'t have javascript enabled. Please activate it for a full experience :D',
-      dropHere : 'Browse an image or drag it here.',
-      colorFromWebsite : 'Get color from website',
-      noSupportForCanvas : 'Your browser does not support the HTML5 canvas tag.',
-      footerText : 'Built with ❤ by ',
-      forText : 'for',
-      exportPalette : 'Export palette',
-      disclaimer : 'en Disclamer',
-      about :  "<h2>10 Kolors a 10KApart project<\/h2> This is a web application to be able to detect the colors from any iamges.",
-      aboutTitle: "About 10 Kolors Project for 10KApart competition"
-    }
-  }
-  if(getLang(request) === "it") {
-    toReturn = {
-      lang : 'it',
-      title : '10Kolors - no more secrets in your images',
-      baseline : 'No more secret Kolors in your images!',
-      noJs : 'You don\'t have javascript enabled. Please activate it for a full experience :D',
-      dropHere : 'Browse an image or drag it here.',
-      colorFromWebsite : 'Get color from website',
-      noSupportForCanvas : 'Your browser does not support the HTML5 canvas tag.',
-      footerText : 'Creato con ❤ da ',
-      forText : 'for',
-      exportPalette : 'Export palette',
-      disclaimer : 'it Disclamer',
-      about :  " ",
-       aboutTitle: "About 10 Kolors Project for 10KApart competition"
+      lang: 'fr',
+      HomeDescription: '',
+      title: '10Kolors - plus de secrets pour vos images',
+      baseline: 'Plus de Kolors secrètes dans vos images!',
+      noJs: 'JavaScript ne semble pas activé. Merci de l\'activer pour une meilleure expérience :D',
+      dropHere: 'Glissez-déposez une image ici.',
+      colorFromWebsite: 'Retrouver les couleurs d\'un site',
+      placeholdInput: "an http://website or an http://image_link",
+      btnKolorize: 'Kolorise!',
+      noSupportForCanvas: 'Votre navigateur ne supporte pas le tag HTML5 canvas.',
+      footerText: 'Fait avec ❤ par ',
+      forText: 'pour',
+      exportPalette: 'Exporter palette',
+      disclaimer: 'fr Disclamer',
+      about: 'fr About',
+      aboutTitle: "About 10 Kolors Project for 10KApart competition",
 
+      disclaimFooter: 'Disclaimer',
+      github: 'View this project on',
+      menuAbout: 'About',
+      menuSettings: 'Settings',
+      menuExports: 'Export',
+      menuKolorize: 'Kolorize',
+      menuRezult: 'Rezult'
+    }
+  }
+  if (getLang(request) === "en") {
+    toReturn = {
+      lang: 'en',
+      HomeDescription: 'With 10 Kolors you know what colors make up an image online, a web page or your local picture.',
+      title: '10Kolors - no more secrets in your images',
+      baseline: 'No more secret Kolors in your images!',
+      noJs: 'You don\'t have javascript enabled. Please activate it for a full experience :D',
+      dropHere: 'Browse an image or drag it here.',
+      colorFromWebsite: 'Get color from a website or an online image',
+      placeholdInput: "an http://website or an http://image_link",
+      btnKolorize: 'Kolorise!',
+      noSupportForCanvas: 'Your browser does not support the HTML5 canvas tag.',
+      footerText: 'Built with ❤ by ',
+      forText: 'for',
+      exportPalette: 'Export palette',
+      disclaimer: 'en Disclamer',
+      about: "<h2>10 Kolors a 10KApart project<\/h2> This is a web application to be able to detect the colors from any iamges.",
+      aboutTitle: "About 10 Kolors Project for 10KApart competition",
+
+      disclaimFooter: 'Disclaimer',
+      github: 'View this project on',
+      menuAbout: 'About',
+      menuSettings: 'Settings',
+      menuExports: 'Export',
+      menuKolorize: 'Kolorize',
+      menuRezult: 'Rezult'
+    }
+  }
+  if (getLang(request) === "it") {
+    toReturn = {
+      lang: 'it',
+      HomeDescription: 'Grazie a 10 Kolors puoi sapere quali colori compongono un\'immagine online, una pagina web o una tua immagine locale.',
+      title: '10Kolors - no more secrets in your images',
+      baseline: 'Nessun  più colore segreto nelle immagini!',
+      noJs: 'You don\'t have javascript enabled. Please activate it for a full experience :D',
+      dropHere: 'Sfoglia un\'immagine o trascinarla qui.',
+      colorFromWebsite: 'Ottenere colori di un sito web o un\'immagine online',
+      placeholdInput: "un http://sitoweb o un http://immagine_online",
+      btnKolorize: 'Kolorizza!',
+      noSupportForCanvas: 'Your browser does not support the HTML5 canvas tag.',
+      footerText: 'Creato con ❤ da ',
+      forText: 'for',
+      exportPalette: 'Esporta palette colori',
+      disclaimer: 'Disclamer',
+      about: "<h2>10 Kolors a 10KApart project<\/h2> This is a web application to be able to detect the colors from any iamges.",
+      aboutTitle: "About 10 Kolors Project for 10KApart competition",
+
+      disclaimFooter: 'Disclaimer',
+      github: 'Guarda questo progetto su',
+      menuAbout: 'Riguardo 10K',
+      menuSettings: 'Impostazioni',
+      menuExports: 'Esporta',
+      menuKolorize: 'Kolorizza',
+      menuRezult: 'Rizultato'
     }
   }
   toReturn[extraKey0] = extraValue0;
@@ -231,7 +263,7 @@ function getLang(request) {
   if (paths.length > 1 && paths[1].length === 2) {
     console.log("RETURN = " + paths[1]);
     return paths[1];
-  } 
+  }
   return "en";
 }
 
